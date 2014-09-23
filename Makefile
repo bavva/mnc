7be2c33bc4d358dffb06478ef044b2765b07d9c7
Bharadwaj Avva <bavva@timberlake.cse.buffalo.edu>
2014-09-23 00:09:03 -0400

    using MNC given template

--- Makefile
+++
@@ -1,5 +0,0 @@
-all:
-	g++ main.cpp -o assignment
-
-clean:
-	rm *.o assignment
---
+++ bavva/src/bavva_assignment1.cpp
@@ -0,0 +1,73 @@
+/**
+ * @bavva_assignment1
+ * @author  Bharadwaj Avva <bavva@buffalo.edu>
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @section DESCRIPTION
+ *
+ * This contains the main function. Add further description here....
+ */
+#include <iostream>
+#include <cstdlib>
+
+#include "../include/global.h"
+
+using namespace std;
+
+// global options
+bool appIsServer = false;
+int appPort = 0;
+
+const int correctParamCount = 2;
+
+void printUsageAndExit(void)
+{
+    printf("Usage: assignment1 <s|c> <port>\n");
+    exit(0);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != correctParamCount + 1)
+        printUsageAndExit();
+
+    // first parameter validation. can be either s or c
+    std::string appMode(argv[1]);
+    if (appMode == "s" || appMode == "S")
+        appIsServer = true;
+    else if (appMode == "c" || appMode == "C")
+        appIsServer = false;
+    else
+        printUsageAndExit();
+
+    // second parameter validation. should be a port number
+    appPort = atol(argv[2]);
+    if (appPort <= 0 || appPort > 65535)
+        printUsageAndExit();
+
+    if (appIsServer)
+    {
+        FSServer *server = new FSServer(appPort);
+        server->start();
+    }
+    else
+    {
+        FSClient *client = new FSClient(appPort);
+        client->start();
+    }
+
+    return 0;
+}
